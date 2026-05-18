92c77a88877720a5b56fd10410cf5e0ad1a99e9d
stblt <staniguchi@bltinc.co.jp>
2026-05-18 17:26:21 +0900

    add: heroセクションのスタイル指定

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const image = document.querySelector('.hero__favorite-images');
 const leftButton = document.querySelector('.hero__favorite-button--left');
 const rightButton = document.querySelector('.hero__favorite-button--right');
+const dots = document.querySelectorAll(".hero__favorite-dot");
 let currentIndex = 0;
 
 function updateImage() {
-    const dots = document.querySelectorAll(".hero__favorite-dot");
     dots.forEach((dot) => {
         dot.classList.remove("hero__favorite-dot--active");
     });
     dots[currentIndex].classList.add("hero__favorite-dot--active");
-    const offset = -currentIndex * 150;
+    const style = window.getComputedStyle(image.children[0]);
+    const marginRight = parseInt(style.marginRight);
+    const imageWidth = image.children[0].offsetWidth + marginRight;
+    image.style.transform = `translateX(-${imageWidth * currentIndex}px)`;
     image.style.transform = `translateX(${offset}%)`;
 };
 
@@ -28,3 +31,10 @@ setInterval(() => {
     currentIndex = (currentIndex + 1) % image.children.length;
     updateImage();
 }, 3000);
+
+dots.forEach((dot) => {
+    dot.addEventListener('click', () => {
+        currentIndex = parseInt(dot.getAttribute('data-slide'));
+        updateImage();
+    });
+});
